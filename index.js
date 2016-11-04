b140ed4ba0cad3b318f87e2f4a7af5f9139256ba
Ahmed Mahmud <ahmedmahmud@Ahmeds-MBP.home>
2016-11-04 12:52:16 -0700

    commit 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express();
-var port = process.env.port || 8081
+var port = process.env.port || 80
 app.use(express.static(__dirname));
 app.get('*', function(req, res) {
   res.sendFile('index.html', { root: __dirname });
