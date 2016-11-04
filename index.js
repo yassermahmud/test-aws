6dcd15930281dd218a2a5c2d6eaef18eeade3975
yasser mahmud <yassermahmud@users.noreply.github.com>
2016-11-04 14:10:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express();
-var port = process.env.port || 80
+var port = 80
 app.use(express.static(__dirname));
 app.get('*', function(req, res) {
   res.sendFile('index.html', { root: __dirname });
