5ecfc888f37103a2fdd827e7c105745dd542cec5
yasser mahmud <yassermahmud@users.noreply.github.com>
2016-11-05 01:43:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.get('*', function(req, res) {
   res.sendFile('index.html', { root: __dirname });
 });
 app.listen(port, function() {
-  console.log('Server is listening on ' + port);
+  console.log('Server is listen on ' + port);
 });
